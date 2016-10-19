94640bd3d999469666845b61218bcb21c3ccb459
IPCedTester <lstickler@ipced.com>
2016-10-19 15:25:50 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,293 @@
+﻿/**
+ * @name Makeaclickablemap Clickable map widget script
+ * @author Tamas Hajdu <tamas.hajdu.mapping@gmail.com>
+ * @copyright Tamas Hajdu 2016
+ * @version 1.0.2
+ * @requires library:jQuery, library:raphael.js, library:qTip.js
+ * @description This script implements a clickable map functionality
+ *  where each region or point can be customized using various settings, like fillColor, hoverColor etc.
+ *  and example can be found below, that would give you an overview of the settings one can use to customize the map
+ */
+var makeaClickableMap = (function(window,$){
+	var _self = {
+	    "configSrc": "js/config.json",
+		"settings": {
+		    "interaction": {
+			    "container": "map"
+			}
+		}
+	};
+	
+	function _init(domContainer){
+	    if(typeof domContainer !== 'undefined'){
+		    /*Cool, domContainer is not empty. Lets check what it is*/
+			switch(true){
+			    case (domContainer instanceof jQuery):
+				    _self.settings.interaction.container = domContainer.get(0);
+				break;
+				case (domContainer instanceof HTMLElement):
+				    _self.settings.interaction.container = domContainer;
+				break;
+				case (typeof domContainer === "string"):
+				    _self.settings.interaction.container = domContainer;
+				break;
+			}
+		}
+	    $.getJSON(_self.configSrc, _startDrawing);
+	}
+	
+	function _startDrawing(data){
+		_self.settings = $.extend({}, _self.settings, data);
+        /*Append an object named "map" to the main settings object*/
+        /*This will hold our Raphael map and all its thingies*/
+        _self.settings = $.extend({}, _self.settings, {map: {glow: false, stroketypes:{'solid': '', 'dashed':'- ', 'none':''}}});
+        _createMap();	    
+	}
+	
+	function _createMap(){
+		/*Set up the map into a container specified*/
+		_self.settings.map.canvas = new Raphael(_self.settings.interaction.container, "100%", "100%");
+        _self.settings.map.canvas.safari();
+        _self.settings.map.canvas.setViewBox(0, 0, _self.settings.size.width, _self.settings.size.height, true);
+        _populateMap();	
+	}
+	
+	function _setBackground(){
+        if(_self.settings.background.transparent){
+            if(_self.settings.interaction.container instanceof jQuery){
+			    _self.settings.interaction.container.addClass('transparent');
+			}
+			else
+			{
+			    if(typeof _self.settings.interaction.container === "string"){
+				    $("#" + _self.settings.interaction.container).addClass('transparent');
+				}
+				else
+				{
+				    $(_self.settings.interaction.container).addClass('transparent');
+				}				
+			}	
+        }
+        else
+        {
+            if(_self.settings.interaction.container instanceof jQuery){
+			    _self.settings.interaction.container.removeClass('transparent').css({'background-color': _self.settings.background.fill});
+			}
+			else
+			{
+			    if(typeof _self.settings.interaction.container === "string"){
+				    $("#" + _self.settings.interaction.container).removeClass('transparent').css({'background-color': _self.settings.background.fill});
+				}
+                else
+                {
+				    $(_self.settings.interaction.container).removeClass('transparent').css({'background-color': _self.settings.background.fill});
+                }						    
+			}			          
+        }	
+	}
+	
+	function _populateMap(arg){
+        
+        if (typeof arg !== 'undefined') { 
+		    _self._config = arg; 
+		}
+		
+        /*Set the background of the map*/
+        _setBackground();
+
+		if(typeof _self._config !== 'undefined'){
+			for(i=0;i<_self.settings.content.length;i++){
+				var current = _self.settings.content[i];
+
+				var result = _self._config.filter(function( obj ) {
+						return obj.name == current["name"];
+				});			
+				
+				if(typeof result[0] !== 'undefined'){
+					if(result[0]["name"] === current["name"] ){
+						current = $.extend({}, current, result[0]);
+						_self.settings.content[i] = $.extend({}, current);
+					}
+				}
+			}	
+        }
+
+        /*Loop through the areas and apply necessary settings*/
+        for(i=0;i<_self.settings.content.length;i++){
+            var current = _self.settings.content[i];
+			
+            /*Create a Raphael path from svg path provided and appeand to the current object*/
+            
+			if(typeof current["area"] === "undefined"){
+			    current["area"] = _self.settings.map.canvas.path(current.svg);
+            }
+			
+            /*Also store the DOM representation of the area*/
+            if(typeof current["areadom"] === "undefined"){
+			    current["areadom"] = current.area[0];
+			}
+            
+            /*Create the labels*/
+            if(current.label.active){
+                if(typeof current["rlabel"] === "undefined"){
+				    current["rlabel"] = _self.settings.map.canvas.text(current.label.posx, current.label.posy, current.label.text);
+				}
+            }
+            
+			
+            /*Assign fill and stroke styles to the area*/
+            _assignStyle("area", current);
+            
+
+            /*Handle events like mouseover, touchstart etc.*/
+            _handleEvents(current);
+        }
+		
+		for(i=0;i<_self.settings.frames.length;i++){
+            var currentFrame = _self.settings.frames[i];
+			currentFrame["area"] = _self.settings.map.canvas.path(currentFrame.svg);
+			_assignStyle("frame", currentFrame);
+		}
+        _self.settings.map.canvas.renderfix();	
+	}
+	
+	function _assignStyle(type, current){
+		switch(type){
+		    case "area":          
+                current["area"].attr({"fill": current.fill, "stroke": current.strokefill, "stroke-width": current.strokewidth, "stroke-dasharray": _self.settings.map.stroketypes[current.stroketype], "stroke-linejoin":"round", "fill-rule": "evenodd"});				
+
+                if(current.hasOwnProperty('rlabel')){
+                   current["rlabel"].attr({"font-size": current.label.size, "fill": current.label.fill}); 
+                }
+        
+		        //Apply style 'inactive' if the area is marked inactive
+                if(!current.active){
+                    current["area"].attr({'fill': current['inactivefill']});
+                }			
+			break;
+			case "frame":
+			    current["area"].attr({"fill-opacity": 0});
+			break;
+		}	
+	}
+	
+	function _areaHovered(current){
+        if(!current['active']){return;}
+
+        for(i=0;i<_self.settings.content.length;i++){
+            if(_self.settings.content[i]["name"] != current["name"]){
+                _self.settings.content[i]['area'].toBack();
+            }
+        }
+        if(typeof current["area"] !== "undefined"){
+		    current['area'].attr({cursor: "pointer"});
+		}
+        if(typeof current["rlabel"] !== "undefined"){		
+		    current['rlabel'].attr({cursor: "pointer"});
+		}	
+        
+        if(current["glow"]){
+            if(!_self.settings.map.glow){
+                _self.settings.map.glow = current['area'].glow({color: current.strokefill, width: 10});
+                current['area'].animate({fill: current.hoveredfill}, 300);
+            }
+        }
+        else
+        {
+            current['area'].animate({fill: current.hoveredfill, "stroke-width": current["stroke-hoveredwidth"]}, 300);
+        }
+        if(current.hasOwnProperty('rlabel')){
+            current['rlabel'].animate({'fill': current.label.hoveredfill});
+        }	    
+	}
+	
+	function _areaLeft(current){
+        if(!current['active']){return;}
+        current['area'].animate({"fill": current.fill, "stroke-width": current["strokewidth"]}, 300);
+		
+        if(_self.settings.map.glow){
+            _self.settings.map.glow.remove();
+            _self.settings.map.glow = false;
+        }
+        if(current.hasOwnProperty('rlabel')){
+            current['rlabel'].animate({'fill': current.label.fill});
+        }	
+	}
+	
+	function _followUrl(urlobj){
+        if(urlobj.openinnew){
+            window.open(urlobj.text);
+        }
+        else
+        {
+             window.top.location =  urlobj.text;
+        }	
+	}
+	
+	function _handleEvents(current){
+        current['area'].mouseover(function(){
+            (function(a){
+                _areaHovered(a);  
+            })(current);
+        });
+        current['area'].mouseout(function(){
+            (function(a){
+                _areaLeft(a);
+            })(current);
+        });
+        current['area'].touchstart(function(){
+            (function(a){
+                _areaHovered(a);
+            })(current);
+        });
+        current['area'].touchend(function(){
+            (function(a){
+                _areaLeft(a);
+            })(current);
+        });    
+        current['area'].click(function(){
+            (function(a){
+                if(a.url.text != ''){
+                    _followURL(a.url);
+                }
+            })(current);
+        });
+        
+        if(current.hasOwnProperty('rlabel')){
+            current['rlabel'].mouseover(function(){
+                (function(a){
+                    _areaHovered(a);
+                })(current);
+            });
+            current['rlabel'].mouseout(function(){
+                (function(a){
+                    _areaLeft(a);
+                })(current);
+            });
+        }
+        
+        //Display tooltip if set to active    
+        if(current.tooltip.active){
+            $(current['area'][0]).qtip({
+                content:current.tooltip.text,
+                position: {
+                    target: 'mouse',
+                    adjust: { x: 20, y: 20 },
+                    viewport: $(window)
+                },
+                style: {
+                    classes: 'qtip-bootstrap',
+                    tip:{
+                        corner: false
+                    }
+                }
+            });
+        }	
+	}
+	
+	return {
+	    initialize: _init,
+		draw: _populateMap
+	}
+	
+})(window, jQuery);
---
+++ testpage.php
@@ -0,0 +1,234 @@
+<!DOCTYPE php>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<link rel="icon" href="assets/icons/favicon-new.ico" type="image/x-icon"/>
+	<!-- Clickable Map Inject -->
+			<link href="//cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.min.css" rel="stylesheet" />
+		<link href="css/style.makeaclickablemap.css" rel="stylesheet" />
+		
+		<!-- End Map -->
+    <title>IPCed.com - Learning Center</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+	
+	<!-- Tipue Search Plugin -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400|Merriweather:300,300italic" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="tipuesearch/tipuesearch.css">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+	<!-- Chat Box -->
+	
+	
+	
+	
+	
+<!-- Start VideoLightBox.com HEAD section --> 
+ <?php include ("vlbHead.php"); ?>
+
+<body>
+ <!-- Navigation -->
+<?php include ("nav.php");?>
+<!-- Carousel Header -->
+<?php include ("CarouselHeader.php");?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" style="text-align:center;">
+                   Administrator Documentation Finder
+                </h1>
+<div class="col-lg-12" style="padding-left:10%">
+<!--<iframe src="http://www.makeaclickablemap.com/map.php?8a0d3ad34fd5f7558367129cdda59d1a6b82a7cb" frameborder="0" scrolling="no" height="720" width="960"></iframe> -->
+<div id="map"></div>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.4/raphael-min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.min.js"></script>
+		<script src="js/app.js"></script>
+		<script>
+			makeaClickableMap.initialize(document.getElementById("map"));	
+		</script>
+<hr>
+	<!-- End VideoLightBox.com BODY section --></div>
+	
+	
+	
+	
+	<h2 style="text-align:center;" >Full Online Campus Training</h2>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i>Admin Orientation Registration</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Sign up for an Administrators Orientation. These are no cost orientations to help you as the Administrator of your company. Fundamental Orientations are available Monday and Wednesday at 9:30 AM and 2:00 PM PST. Advanced Orientation is Friday at 9:30 AM PST.</p>
+                        <a href="https://attendee.gotowebinar.com/rt/8937491687506028289" class="btn btn-default">Sign Up Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-gift"></i>Administrator Training</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>From here you can learn how to do everything from resetting a users password to creating your own Custom Qualifications. We highly suggest that this resource be accessed regularly to keep up-to-date with all of the possibilities that can be acheived with your subscription.</p>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-compass"></i>User Training</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Here are resources you can give your Users to help them understand their Campus. We use this resource to train new hires and update anyone having trouble navigating their training. It's great to help users print their own certificates, resetting their password, and access their account.</p>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Quick Admin Guides (PDF)</h2>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="assets/PDF/Managing%20User%20Accounts.pdf" target="_blank">
+                    <img class="img-responsive img-portfolio img-hover" src="assets/PDF/ManagingUserAccounts.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="assets/PDF/Assigning%20Training.pdf" target="_blank">
+                    <img class="img-responsive img-portfolio img-hover" src="assets/PDF/AssignTraining.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="assets/PDF/How%20to%20Assign%20the%20Administration%20Role.pdf" target="_blank">
+                    <img class="img-responsive img-portfolio img-hover" src="assets/PDF/AssignAdmin.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="assets/PDF/Credit%20Training%20Event.pdf" target="_blank">
+                    <img class="img-responsive img-portfolio img-hover" src="assets/PDF/CreditTrainingEvent.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="assets/PDF/Create%20a%20Custom%20Qualification.pdf" target="_blank">
+                    <img class="img-responsive img-portfolio img-hover" src="assets/PDF/CreateACustomQualification.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="assets/PDF/How%20to%20View%20Inactive%20Users.pdf" target="_blank">
+                    <img class="img-responsive img-portfolio img-hover" src="assets/PDF/HowtoViewInactiveUsers.png" alt="">
+                </a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Full <script>document.write(new Date().getFullYear())</script> Administrators Training Manual</h2>
+            </div>
+            <div class="col-md-6">
+                <p>The <script>document.write(new Date().getFullYear())</script> Administrators Training Manual can help with:</p>
+                <ul>
+                    <li><strong>Setting up new users</strong>
+                    </li>
+                    <li>Creating Custom Qualifications</li>
+                    <li>Resetting User Passwords</li>
+                    <li>Running Reports and Certificates</li>
+                    <li>Managing and Assigning Training</li>
+                    <li>Assigning Administrators</li>
+					<li>Working with groups and teams</li>
+					<li>Editing your company</li>
+					<li>Crediting Live Training</li>
+                </ul>
+                <p>The <script>document.write(new Date().getFullYear())</script> Administrators Training Manual has all of the information you will need to run your Online Campus efficiently from day one. This allows you to be the most current and up-to-date with all of your regulatory needs.</p>
+            </div>
+            <div class="col-md-6">
+                <a href="assets/PDF/2016%20IPCed%20Administrator%20Guide.pdf" target="_blank">
+                    <img class="img-responsive img-portfolio img-hover" src="assets/PDF/AdminGuide.png" alt="">
+                </a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <h2>For even more help, you can <a href="mailto:help@ipced.com?Subject=Learning%20Center%20Help%20Requested"> Email Us</a>,</br>call us at (877) 843-8374, or <a href="#" onclick="chatPop()">Chat Now</a>!</h2>
+                </div>
+                <div class="col-md-4">
+                    <a href="#" class="chat-button" onclick="chatPop()"><img style="margin-left: 15%;" src="/assets/icons/chatIcon.png" alt="Live Chat"></a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; 2004 - <script>document.write(new Date().getFullYear())</script> Institute for Professional Care Education&reg; </p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+<script>
+function chatPop() {
+    var myWindow = window.open("https://home-c17.incontact.com/inContact/ChatClient/ChatClient.aspx?poc=2cb0be9b-bd8a-4719-8e72-3561971f4795&bu=4595508", "", "width=495,height=520");
+}
+</script>
+</body>
+
+
+</body>
+
+</html>
